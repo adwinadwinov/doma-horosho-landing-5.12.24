539b92d487b822f659a7845dcb8fc1e4159ed682
Alexander Thorik <a.thorik@yandex.ru>
2024-12-10 13:55:58 +0300

    refactor: упрощен js табов

--- src/tabs.js
+++ src/tabs.js
@@ -1,12 +1,12 @@
 export class Tabs {
-  #_tabComponent;
-  #_tabPanels;
-  #_tabTriggers;
-  #_currentTab;
-  #_indicatorSlide;
+  _tabComponent;
+  _tabPanels;
+  _tabTriggers;
+  _currentTab;
+  _indicatorSlide;
 
   constructor({ tabSelector, repaintOnresize }) {
-    this.#_tabComponent = document.querySelector(tabSelector || ".tabs");
+    this._tabComponent = document.querySelector(tabSelector || ".tabs");
     this._initTabs();
     if (repaintOnresize) {
       this._resizeControl();
@@ -14,13 +14,13 @@ export class Tabs {
   }
 
   _initTabs() {
-    this.#_tabTriggers = Array.from(this.#_tabComponent.querySelectorAll(".tabs-triggers__btn"));
-    this.#_tabPanels = Array.from(this.#_tabComponent.querySelectorAll(".tabs-content__panel"));
-    this.#_indicatorSlide = this.#_tabComponent.querySelector(".tabs-indicator__slide");
-    this.#_currentTab = this.#_tabTriggers.find((trigger) => trigger.dataset.value === this.#_tabComponent.dataset.defaultValue);
-    this._changeActiveTab(this.#_currentTab);
+    this._tabTriggers = Array.from(this._tabComponent.querySelectorAll(".tabs-triggers__btn"));
+    this._tabPanels = Array.from(this._tabComponent.querySelectorAll(".tabs-content__panel"));
+    this._indicatorSlide = this._tabComponent.querySelector(".tabs-indicator__slide");
+    this._currentTab = this._tabTriggers.find((trigger) => trigger.dataset.value === this._tabComponent.dataset.defaultValue);
+    this._changeActiveTab(this._currentTab);
 
-    this.#_tabTriggers.forEach((tabTrigger) => {
+    this._tabTriggers.forEach((tabTrigger) => {
       tabTrigger.addEventListener("click", (e) => {
         this._changeActiveTab(e.target);
       });
@@ -29,7 +29,7 @@ export class Tabs {
 
   _resizeControl() {
     window.addEventListener("resize", () => {
-      this._handleIndicator(this.#_currentTab);
+      this._handleIndicator(this._currentTab);
     });
   }
 
@@ -37,13 +37,13 @@ export class Tabs {
     if (trigger.classList.contains("tabs-triggers__btn--active")) return;
 
     const value = trigger.dataset.value;
-    const targetTab = this.#_tabPanels.find((panel) => panel.dataset.value === value);
-    this.#_tabPanels.forEach((panel) => {
+    const targetTab = this._tabPanels.find((panel) => panel.dataset.value === value);
+    this._tabPanels.forEach((panel) => {
       if (panel.classList.contains("tabs-content__panel--active")) {
         panel.classList.remove("tabs-content__panel--active");
       }
     });
-    this.#_tabTriggers.forEach((trigger) => {
+    this._tabTriggers.forEach((trigger) => {
       if (trigger.classList.contains("tabs-triggers__btn--active")) {
         trigger.classList.remove("tabs-triggers__btn--active");
       }
@@ -52,12 +52,12 @@ export class Tabs {
     targetTab.classList.add("tabs-content__panel--active");
     trigger.classList.add("tabs-triggers__btn--active");
     this._handleSelectedAccessibility(trigger);
-    this.#_currentTab = trigger;
+    this._currentTab = trigger;
     this._handleIndicator(trigger);
   }
 
   _handleSelectedAccessibility(target) {
-    this.#_tabTriggers.forEach((trigger) => {
+    this._tabTriggers.forEach((trigger) => {
       trigger.ariaSelected = false;
     });
 
@@ -66,10 +66,10 @@ export class Tabs {
 
   _handleIndicator(trigger) {
     document.fonts.ready.then(() => {
-      const width = this.#_currentTab.offsetWidth;
+      const width = this._currentTab.offsetWidth;
       const left = trigger.offsetLeft;
-      this.#_indicatorSlide.style.width = width + "px";
-      this.#_indicatorSlide.style.left = left + "px";
+      this._indicatorSlide.style.width = width + "px";
+      this._indicatorSlide.style.left = left + "px";
     });
   }
 }
