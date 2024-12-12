e5705bb8d92c38d54ceefde4a6a6a24d238b1346
Alexander Thorik <a.thorik@yandex.ru>
2024-12-12 17:34:55 +0300

    feat: Добавлен favicon

--- php/components/head.php
+++ php/components/head.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <link rel="icon" type="image/png" href="/assets/favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
