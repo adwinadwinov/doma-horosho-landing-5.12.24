e84d46fda4ac33668430faa444dd94f04be7f1c2
Alexander Thorik <a.thorik@yandex.ru>
2024-12-12 16:18:49 +0300

    fix: Исправлены лишние взаимодействия с dom

--- src/scroll.js
+++ src/scroll.js
@@ -1,19 +1,23 @@
 export const initScroll = () => {
-  const header = document.querySelector(".js-header");
+  const header = document.querySelector(".header");
 
   Motion.scroll((_, info) => {
     const diff = info.y.velocity;
     const currentScroll = info.y.current;
 
-    if (currentScroll > 80) {
+    if (currentScroll > 80 && !header.classList.contains("header--shadow")) {
       header.classList.add("header--shadow");
-    } else {
+    }
+
+    if (currentScroll <= 80) {
       header.classList.remove("header--shadow");
     }
 
-    if (diff > 0 && info.y.current > 80) {
+    if (diff > 0 && info.y.current > 80 && !header.classList.contains("header--scroll-hidden")) {
       header.classList.add("header--scroll-hidden");
-    } else {
+    }
+
+    if (diff < 0) {
       header.classList.remove("header--scroll-hidden");
     }
   });
